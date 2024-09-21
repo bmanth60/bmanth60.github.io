cb9e153bce43b135d04e9194b9698e71732b582c
bmanth60 <5578150+bmanth60@users.noreply.github.com>
2024-09-20 17:43:26 -0700

    Add aws-cdk for bucket creation and management

---
+++ aws-cdk/lib/aws-cdk-stack.ts
@@ -0,0 +1,51 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as iam from "aws-cdk-lib/aws-iam";
+
+export class AwsCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The bucket name must match the domain name since
+    // s3 proxy requires host name to match bucket name
+    // and Host override is a cloudflare enterprise only feature
+    const bucketName = "aws.seaotterstudio.com";
+
+    const logsBucket = new s3.Bucket(this, "LogsBucket", {
+      bucketName: `${bucketName}-logs`,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
+
+    // Define policy to deny access log bucket
+    const denyAll = new iam.PolicyStatement({
+      effect: iam.Effect.DENY,
+      actions: ["s3:*"],
+      resources: [logsBucket.arnForObjects("*")],
+      principals: [new iam.AnyPrincipal()],
+    });
+
+    logsBucket.addToResourcePolicy(denyAll);
+
+    const siteBucket = new s3.Bucket(this, "SiteBucket", {
+      bucketName,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
+      enforceSSL: true,
+      websiteIndexDocument: "index.html",
+      serverAccessLogsBucket: logsBucket,
+      serverAccessLogsPrefix: "access-logs",
+      minimumTLSVersion: 1.2,
+    });
+
+    // Define policy to allow public read access
+    const publicRead = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      principals: [new iam.AnyPrincipal()],
+      actions: ["s3:GetObject"],
+      resources: [siteBucket.arnForObjects("*")],
+    });
+
+    siteBucket.addToResourcePolicy(publicRead);
+  }
+}
---
+++ aws-cdk/test/aws-cdk-stack.test.ts
@@ -0,0 +1,18 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdk from '../lib/aws-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-stack.ts
+describe("stack", () => {
+  it("should test a resource", () => {
+    //   const app = new cdk.App();
+    //     // WHEN
+    //   const stack = new AwsCdk.AwsCdkStack(app, 'MyTestStack');
+    //     // THEN
+    //   const template = Template.fromStack(stack);
+    //   template.hasResourceProperties('AWS::SQS::Queue', {
+    //     VisibilityTimeout: 300
+    //   });
+  });
+});
